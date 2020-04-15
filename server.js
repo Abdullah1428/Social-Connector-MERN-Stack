fd2db04d5e6ccd9a23d950801a2988a2e8f4e781
Abdullah <abdullah>
2020-04-15 17:05:22 +0500

    routes for api added

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const connectDB = require("./config/db");
 const app = express();
 
 // Connect to the Database
-connectDB();
+//connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
