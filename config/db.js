c105546e90890c6a884f2959af450d93960fe91d
Abdullah <abdullah>
2020-04-15 16:16:25 +0500

    database connected

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+// this returns a promise
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err.message);
+    // exits process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
